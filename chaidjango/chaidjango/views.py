5c5c61127c7758e46d248f6152d4c5f056e98647
ridamsinghal25 <ridamsinghal25@gmail.com>
2025-07-15 07:27:24 +0530

    add the chaidjango project to codebase

---
+++ chaidjango/chaidjango/views.py
@@ -0,0 +1,17 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+def home(request):
+    # return HttpResponse("Hello, world. You are at chai aur Django Home Page")
+
+    return render(request, "website/index.html")
+
+def about(request):
+    # return HttpResponse("Hello, world. You are at chai aur Django About Page")
+
+    return render(request, "website/about.html")
+
+def contact(request):
+    # return HttpResponse("Hello, world. You are at chai aur Django Contact Page")
+
+    return render(request, "website/contact.html") 
